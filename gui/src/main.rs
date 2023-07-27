7ecbc2e132c4ef8c82d3f3a589c428c54886f3bf
Ethan Plumb <ethan.plumb@keysight.com>
2023-07-27 15:08:30 -0700

    baby gui

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,3 +1,35 @@
+use druid::widget::{Container, Flex, Label, List, Split};
+use druid::{AppLauncher, Color, Widget, WindowDesc};
+use im::Vector;
+
+type TodoList = Vector<String>;
+
+fn build_ui() -> impl Widget<()> {
+    Split::columns(
+        Container::new(
+            Flex::column()
+                .with_flex_child(Label::new("first item"), 1.0)
+                .with_flex_child(Label::new("second item"), 1.0)
+                .with_flex_child(Label::new("third item"), 1.0)
+                .with_flex_child(Label::new("fourth item"), 1.0),
+        )
+        .border(Color::grey(0.6), 2.0),
+        Container::new(
+            Flex::column()
+                .with_flex_child(Label::new("Button placeholder"), 1.0)
+                .with_flex_child(Label::new("Textbox placeholder"), 1.0),
+        )
+        .border(Color::grey(0.6), 2.0),
+    )
+}
+
 fn main() {
-    println!("Hello, world!");
+    let main_window = WindowDesc::new(build_ui())
+        .window_size((600.0, 400.0))
+        .title("Gui Shapes");
+    let initial_data = ();
+
+    AppLauncher::with_window(main_window)
+        .launch(initial_data)
+        .expect("Failed to launch application");
 }
